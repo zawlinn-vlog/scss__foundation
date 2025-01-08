cbaa9fea16d8207e73de6d74b778bbb1a520c2ff
zawlinn-vlog <zawlinn.profile@outlook.com>
2025-01-08 21:16:06 +0700

    SCSS: Natour Project

---
+++ 01 Natours Project/assets/js/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// const obj = {
+//   name: "Zaw Linn",
+//   age: 33,
+// };
+
+// const arr = [1, 3, 54, 52, 62, 234, 5, 232, 5];
+
+// for (const x in obj) {
+//   console.log(obj[x]);
+// }
+
+// for (const x of arr) {
+//   console.log(x);
+// }
